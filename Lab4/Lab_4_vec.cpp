4e7aa65d585daf0343f2c908ad2e9244d78243a7
ArghyoC <145810116+ArghyoC@users.noreply.github.com>
2023-10-29 13:40:26 -0700

    Add files via upload

---
+++ Lab4/Lab_4_vec.cpp
@@ -0,0 +1,35 @@
+/***
+ * CSE 2010 Fall 2023
+ * Lab 4
+ * Arghyo Chakma #007873847
+ * 10/27/2023
+ *
+ * Students will learn how to organize data structures, arrays and vectors. 
+ * Learn the similarities and differences between the two in terms of how they contain, access, and updates element as well as to how to pass them into functions.
+ *  Understand how these data structures allocate memory and their relation to pointers.
+ *
+ * {Describe 1) what was most challenging and 2) how you solved/overcame the problem OR
+ * the directions of how you PLAN to solve it}
+ *
+***/
+
+#include <iostream>
+#include "ArrayVec.h"
+#include "DisplayVec.h"
+using namespace std;
+
+
+
+int main()
+{
+    // initialize vec
+
+    //initialize ArrayVec and DisplayVec
+
+
+    // populate with values
+
+    // display contents
+
+}
+
